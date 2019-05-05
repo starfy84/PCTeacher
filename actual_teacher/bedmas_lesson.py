bb0a14867134976eb72a339dcb2bcc6fa6c3b010
Harit Kapadia <haritkapadia@rocketmail.com>
2019-05-04 21:31:47 -0400

    Created question generation

--- actual_teacher/bedmas_lesson.py
+++ actual_teacher/bedmas_lesson.py
@@ -8,39 +8,20 @@ def show_lesson(lesson):
         for example in examples:
             print(example['title'])
             print(example_generation(example['generation']))
+            question_generation(example['generation'])
     else:
         for content in lesson['content']:
             show_lesson(content)
 
-# s has to be a valid variable or a number
-# returns value as a string
-def get_value(s, variables):
-    out = s[2:-1];
-    m = re.fullmatch('\\w+', out)
-    if m is None:
-        print(m)
-    else:
-        out = str(variables[out])
-    return out
-
-
-def example_generation_old(example):
-    varlist = sorted(example['variables'], key = lambda x : x['order'])
-    vardict = {k['variable']: k['value'] for k in varlist}
-    out = example['expression']
-    match = re.search('\\$\\{([^}]+)\}', out)
-    while match is not None:
-        l, r = match.span()
-        out = out[:l] + get_value(out[l:r], vardict) + out[r:]
-        match = re.search('\\$\\{([^}]+)\}', out)
-    return out
-
-def example_generation(example):
-    varlist = sorted(example['variables'], key = lambda x : x['order'])
+def create_vardict(example):
+    varlist = sorted(example, key = lambda x : x['order'])
     vardict = {}
     for k in varlist:
         vardict[k['variable']] = str(eval(var_replace(k['value'], vardict)))
+    return vardict
 
+def example_generation(example):
+    vardict = create_vardict(example['variables'])
     out = var_replace(example['expression'], vardict)
     match = re.search('\\$\\{([^}]+)\}', out)
     while match is not None:
@@ -48,6 +29,16 @@ def example_generation(example):
         out = out[:l] + str(eval(out[l + 3:r - 2])) + out[r:]
         match = re.search('\\$\\{([^}]+)\}', out)
     return out
+
+def question_generation(example):
+    vardict = create_vardict(example['variables'])
+    out = var_replace(example['expression'], vardict)
+    match = re.search('\\$\\{([^}]+)\}', out)
+    l, r = match.span()
+    print(out[:l] + '?')
+    while input('Answer: ') != str(eval(out[l + 3:r - 2])):
+        print('Try again')
+    print('Correct!')
 
 def var_replace(s, vardict):
     inexp = s.split()
