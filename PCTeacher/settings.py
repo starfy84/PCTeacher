8d46dec840ca63c3df7b98d56427d893ea26c0b5
Dereck Tu <dereck.tu@hotmail.com>
2019-05-05 01:14:22 -0400

    Add login/logout

--- PCTeacher/settings.py
+++ PCTeacher/settings.py
@@ -133,3 +133,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/'
--- PCTeacher/urls.py
+++ PCTeacher/urls.py
@@ -15,12 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from website.views import home, lesson, sublesson
+from django.contrib.auth import views as auth_views
+from website.views import home, lesson, sublesson, register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
+    path('register/', register, name = 'register'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'login.html'),name = 'login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'logout.html'),name = 'logout'),
     path('lesson/<int:id>/', include([
         path('', lesson, name='lesson'),
         path('<int:sub_id>/', sublesson, name='sublesson'),
